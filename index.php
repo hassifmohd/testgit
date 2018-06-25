f94f05307bd768339b0caa3e43eba641a3d18889
hassifmohd <mohdhassif.jabir@tyrellsys.com.my>
2018-06-26 00:02:14 +0800

    should be okay now

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
-echo "HELLO WORLD";
+echo "HELLO WORLD. Study gitflow here: https://jeffkreeftmeijer.com/git-flow/";
 
 echo "authentication V0.1 : THIS FROM git flow release start '0.1'";
 
 echo "this from : GIT FLOW HOTFIX START 'something_went_wrong'";
+
+echo "'git flow hotfix start stillgoterror'  = still got error";
